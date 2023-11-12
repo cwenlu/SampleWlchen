a8a8709aa152c7e89e05430b3236b549cfe189e4
wlchen <cwenlu@sina.cn>
2023-11-12 19:09:53 +0800

    gradle生命周期

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
         eachPlugin {
             //https://docs.gradle.org/current/userguide/custom_plugins.html#note_for_plugins_published_without_java_gradle_plugin
             // 没有使用java-gradle-plugin 插件方式发布缺少标记，需要配置发现策略
-             if("com.wlchen.sample2" == requested.id.id){
+            if ("com.wlchen.sample2" == requested.id.id) {
                 useModule("com.wlchen.sample2:com.wlchen.sample2.gradle.plugin:1.0.0")
             }
         }
@@ -43,3 +43,53 @@ include(":asm-sample")
 include(":plugin-sample:sample1")
 include(":plugin-sample:sample2")
 include(":use-plugin")
+
+
+
+initHook(gradle)
+/**
+ * [构建的生命周期](https://docs.gradle.org/current/userguide/build_lifecycle.html)
+ */
+fun initHook(gradle: Gradle) {
+    //需要多个都监听时，不然可以使用下面单一的
+    gradle.addBuildListener(object : BuildListener {
+        override fun settingsEvaluated(settings: Settings) {}
+
+        override fun projectsLoaded(gradle: Gradle) {}
+
+        override fun projectsEvaluated(gradle: Gradle) {}
+
+        override fun buildFinished(result: BuildResult) {}
+
+    })
+
+    //初始化阶段 start
+    gradle.settingsEvaluated {
+        //此时我们可以针对所有项目做些通用处理
+        println("settings.gradle执行完成,Settings评估完毕")
+    }
+
+    gradle.projectsLoaded {
+        rootProject.allprojects {
+            println("projectsLoaded: " + name)
+        }
+        println("project初始化完成，但各project自己的配置还没执行")
+    }
+
+    //配置阶段的 start
+    gradle.beforeProject {
+        println("beforeProject:${this}")
+    }
+    gradle.afterProject {
+        println("afterProject:${this}")
+    }
+    gradle.projectsEvaluated {
+        println("所有project评估完成")
+    }
+
+    //配置阶段结束后所有task会形成一个有向无环图(dag)
+    gradle.taskGraph.addTaskExecutionGraphListener {
+        println("task执行前")
+    }
+
+}
